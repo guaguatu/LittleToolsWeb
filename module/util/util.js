96fff6e0482d35c70541ee0fa09e13cd6c521897
JoeYuan <yuan.zhaoyi@163.com>
2025-02-24 21:29:50 +0800

    feat: move load functions to util.js

--- module/util/util.js
+++ module/util/util.js
@@ -1,1 +1,32 @@
 
+function getBasePath() {  // 根据后退配置调整
+    let rootPath = window.location.pathname.split('/').slice(0, 2).join('/');
+    // console.log("ROOT PATH: ", rootPath);
+    return rootPath;
+}
+
+function loadScript(scripts) {
+    scripts.forEach(function (scriptPath) {
+        let script = document.createElement('script');
+        script.src = getBasePath() + scriptPath;
+        document.body.appendChild(script);
+    });
+}
+
+function loadCss(stylesheets) {
+    stylesheets.forEach(function (stylePath) {
+        let link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = getBasePath() + stylePath;
+        document.head.appendChild(link);
+    });
+}
+
+// 动态加载 CSS 文件
+function loadCssByUrl(url) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.head.appendChild(link);
+}
